68e17cb32176a9f056a674c2b17d9dc229694012
Joe Bates <joebates@jbimagine.com>
2018-09-09 14:27:29 -0700

    EP-0001 Bookshelves init

---
+++ src/Bookshelves.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class Bookshelves extends React.Component {
+   render() {
+       return
+   }
+}
