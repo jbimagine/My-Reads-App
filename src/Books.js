a930c02bf048ae6d551f35fe84fdacecb7557a16
Joe Bates <joebates@jbimagine.com>
2018-09-15 11:39:38 -0700

    EP-0001 Set key id's for the books

--- src/Books.js
+++ src/Books.js
@@ -8,11 +8,11 @@ class Books extends React.Component {
        const books = this.props.books;
 
        return <ol className = 'books-grid'>
-           {books.map(book => (
+           {books.map((book, index) => (
                <li>
                <div className="book">
                  <div className="book-top">
-                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                   <div key = {index} className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                    <div className="book-shelf-changer">
                      <select>
                        <option value="move" disabled>Move to...</option>
@@ -23,8 +23,8 @@ class Books extends React.Component {
                      </select>
                    </div>
                  </div>
-                 <div className="book-title">{book.title}</div>
-                 <div className="book-authors">{book.author}</div>
+                 <div key = {book.title} className="book-title">{book.title}</div>
+                 <div key = {book.author} className="book-authors">{book.author}</div>
                </div>
              </li>
            ))}
