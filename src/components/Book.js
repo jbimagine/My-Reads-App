4d7e79584a934518aff0756abdfc1fb6a9463f1a
Joe Bates <joebates@jbimagine.com>
2018-09-29 19:48:58 -0700

    Review fixes

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,7 +7,7 @@ class Book extends React.Component {
         <li>
           <div className="book">
         <div className="book-top">
-          <div key = {book.imageLinks} className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.smallThumbnail || ''})` }}></div>
+          <div key = {book.imageLinks} className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail || ''})` }}></div>
           <div className="book-shelf-changer">
             <select value = {book.shelf || "none"} onChange = {(event)=> {this.props.changeShelf(book, event.target.value)}}>
               <option value="move" disabled>Move to...</option>
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Component } from 'react'
-import ReactDOM from 'react-dom'
 import Book from './Book'
 
 
